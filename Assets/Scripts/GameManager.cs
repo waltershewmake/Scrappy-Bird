1e0095d760b7e96d386b10e0539396cc024d913e
Walter Shewmake <waltershewmake@gmail.com>
2024-04-20 09:55:17 -0600

    finish menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     
     public static GameManager Instance;
+
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    [FormerlySerializedAs("gamOver")] public GameObject gameOver;
     
     private int _score;
     
     private void Awake()
     {
+        Pause();
+        
+        Application.targetFrameRate = 60;
+        
         if (Instance == null)
         {
             Instance = this;
@@ -21,13 +32,41 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void Play()
+    {
+        _score = 0;
+        scoreText.text = "0";
+        
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        
+        foreach (Pipes pipe in pipes)
+        {
+            Destroy(pipe.gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
     }
     
     public void IncreaseScore()
     {
         _score++;
+        scoreText.text = _score.ToString();
     }
 }
